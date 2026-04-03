9d9f0d47d40af9d8cd93e0cd23764d1f624fc48c
jellomoon <damonita@gmail.com>
2026-04-03 18:37:22 +0200

    Set up initial file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Weather app</h1>
+      <p>
+        Project coded by Mona Beyersdorf, open sourced on{" "}
+        <a href="" target="_blank">
+          GitHub
+        </a>{" "}
+      </p>
+    </div>
+  );
+}
