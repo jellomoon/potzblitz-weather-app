45dbe15e17e44ac2b7c555f1e1fb8c69774efbc6
jellomoon <damonita@gmail.com>
2026-04-05 17:43:52 +0200

    Created component to convert temperature

--- src/WeatherData.jsx
+++ src/WeatherData.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherData(props) {
   return (
@@ -13,19 +14,16 @@ export default function WeatherData(props) {
         <li className="text-capitalize">{props.data.condition}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-6">
+        <div className="col-7">
           <div className="d-flex align-items-center">
             <WeatherIcon code={props.data.icon} />
             <div className="ms-2">
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°C</span>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
-        <div className="col-6">
-          <ul className="ms-4">
+        <div className="col-5">
+          <ul>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
             <li>Pressure: {props.data.pressure} mbar</li>
